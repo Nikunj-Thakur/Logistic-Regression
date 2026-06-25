0802d33bed3ed5fd6d2015899cafde99cad90992
Nikunj Thakur <nik.thak22@gmail.com>
2026-06-25 17:48:38 +0100

    add files for logistic

---
+++ basic_plot/sigmoid_plot.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+import logistic_utility_function as luf
+
+input_array = np.array([1, 2, 3])
+exp_array = np.exp(input_array)
+
+print("Input to exp:", input_array)
+print("Output of exp:", exp_array)
+
+# Input is a single number
+input_val = 1
+exp_val = np.exp(input_val)
+
+print("Input to exp:", input_val)
+print("Output of exp:", exp_val)
+
+# Generate an array of evenly spaced values between -10 and 10
+z_tmp = np.arange(-10, 11)
+
+# Use the function implemented above to get the sigmoid values
+y = luf.sigmoid(z_tmp)
+
+# Code for pretty printing the two arrays next to each other
+np.set_printoptions(precision=3)
+print("Input (z), Output (sigmoid(z))")
+
+print(np.c_[z_tmp, y])
+
+# Plot z vs sigmoid(z)
+fig, ax = plt.subplots(1, 1, figsize=(5, 3))
+ax.plot(z_tmp, y, c="b")
+
+ax.set_title("Sigmoid function")
+ax.set_ylabel('sigmoid(z)')
+ax.set_xlabel('z')
+plt.show()
