d1d606dada09753974e2f344fb71304303e4ad78
Nikunj Thakur <nik.thak22@gmail.com>
2026-07-02 20:12:33 +0100

    update demo model of 2 features

--- logistic_regression_model.py
+++ logistic_regression_model.py
@@ -10,7 +10,7 @@ b_tmp  = 0.
 alph = 0.1
 iters = 10000
 
-w_out, b_out, _ = luf.gradient_descent(X_train, y_train, w_tmp, b_tmp, alph, iters) 
+w_out, b_out, J_hist = luf.gradient_descent(X_train, y_train, w_tmp, b_tmp, alph, iters) 
 print(f"\nupdated parameters: w:{w_out}, b:{b_out}")
 
 
@@ -26,11 +26,23 @@ ax.scatter(X_train[neg, 0], X_train[neg, 1], marker='o')
 ax.axis([0, 4, 0, 4])
 ax.set_ylabel('$x_1$', fontsize=12)
 ax.set_xlabel('$x_0$', fontsize=12)
-ax.set_title('two variable plot')
+ax.set_title('Two variable plot')
 plt.tight_layout()
 
 x0 = -b_out/w_out[0]
 x1 = -b_out/w_out[1]
 ax.plot([0,x0],[x1,0])
 plt.show()
+
+fig, (ax1, ax2) = plt.subplots(1, 2, constrained_layout=True, figsize=(12, 4))
+ax1.plot(J_hist[:100]) # ax1.plot(range(len(J_hist[:100])), J_hist[:100])
+ax1.set_title("Cost vs. iteration")
+ax1.set_ylabel('Cost')
+ax1.set_xlabel('iteration step')
+
+ax2.plot(100 + np.arange(len(J_hist[100:])), J_hist[100:])
+ax2.set_title("Cost vs. iteration (tail)")
+ax2.set_ylabel('Cost')
+ax2.set_xlabel('iteration step')
+
 plt.show()
--- logistic_utility_function.py
+++ logistic_utility_function.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import copy, math
+import copy
+import math
 
 
 def log_1pexp(x, maximum=20):
@@ -11,13 +12,14 @@ def log_1pexp(x, maximum=20):
     out : (ndarray Shape matches x      output ~= np.log(1+exp(x))
     '''
 
-    output  = np.zeros_like(x,dtype=float)
-    i    = x <= maximum  # e.g. [True,True,True,False]
-    ni   = np.logical_not(i) # e.g. [False,False,False,True]
+    output = np.zeros_like(x, dtype=float)
+    i = x <= maximum  # e.g. [True,True,True,False]
+    ni = np.logical_not(i)  # e.g. [False,False,False,True]
 
-    output[i]  = np.log(1 + np.exp(x[i]))
+    output[i] = np.log(1 + np.exp(x[i]))
     output[ni] = x[ni]
     return output
+
 
 def sigmoid(z):
     """
@@ -49,31 +51,39 @@ def compute_cost_matrix(X, y, w, b, logistic=False, lambda_=0, safe=True):
       total_cost: (scalar)                cost
     """
     m = X.shape[0]
-    y = y.reshape(-1,1)             # ensure 2D
-    w = w.reshape(-1,1)             # ensure 2D
+    y = y.reshape(-1, 1)             # ensure 2D
+    w = w.reshape(-1, 1)             # ensure 2D
     if logistic:
-        if safe:  #safe from overflow
-            z = X @ w + b                                                           #(m,n)(n,1)=(m,1)
+        if safe:  # safe from overflow
+            z = X @ w + b  # (m,n)(n,1)=(m,1)
             cost = -(y * z) + log_1pexp(z)
-            cost = np.sum(cost)/m                                                   # (scalar)
+            # (scalar)
+            cost = np.sum(cost)/m
         else:
-            f    = sigmoid(X @ w + b)                                               # (m,n)(n,1) = (m,1)
-            cost = (1/m)*(np.dot(-y.T, np.log(f)) - np.dot((1-y).T, np.log(1-f)))   # (1,m)(m,1) = (1,1)
-            cost = cost[0,0]                                                        # scalar
+            # (m,n)(n,1) = (m,1)
+            f = sigmoid(X @ w + b)
+            cost = (1/m)*(np.dot(-y.T, np.log(f)) -
+                          np.dot((1-y).T, np.log(1-f)))   # (1,m)(m,1) = (1,1)
+            # scalar
+            cost = cost[0, 0]
     else:
-        f    = X @ w + b                                                        # (m,n)(n,1) = (m,1)
-        cost = (1/(2*m)) * np.sum((f - y)**2)                                   # scalar
+        # (m,n)(n,1) = (m,1)
+        f = X @ w + b
+        # scalar
+        cost = (1/(2*m)) * np.sum((f - y)**2)
 
-    reg_cost = (lambda_/(2*m)) * np.sum(w**2)                                   # scalar
+    # scalar
+    reg_cost = (lambda_/(2*m)) * np.sum(w**2)
 
     total_cost = cost + reg_cost                                                # scalar
 
-    return total_cost     
+    return total_cost
 
-def compute_gradient_logistic(X, y, w, b): 
+
+def compute_gradient_logistic(X, y, w, b):
     """
     Computes the gradient for logistic regression 
- 
+
     Args:
       X (ndarray (m,n): Data, m examples with n features
       y (ndarray (m,)): target values
@@ -83,26 +93,26 @@ def compute_gradient_logistic(X, y, w, b):
       dj_dw (ndarray (n,)): The gradient of the cost w.r.t. the parameters w. 
       dj_db (scalar)      : The gradient of the cost w.r.t. the parameter b. 
     """
-    m,n = X.shape
-    dj_dw = np.zeros((n,))                           #(n,)
+    m, n = X.shape
+    dj_dw = np.zeros((n,))  # (n,)
     dj_db = 0.
 
     for i in range(m):
-        f_wb_i = sigmoid(np.dot(X[i],w) + b)          #(n,)(n,)=scalar
-        err_i  = f_wb_i  - y[i]                       #scalar
+        f_wb_i = sigmoid(np.dot(X[i], w) + b)  # (n,)(n,)=scalar
+        err_i = f_wb_i - y[i]  # scalar
         for j in range(n):
-            dj_dw[j] = dj_dw[j] + err_i * X[i,j]      #scalar
+            dj_dw[j] = dj_dw[j] + err_i * X[i, j]  # scalar
         dj_db = dj_db + err_i
-    dj_dw = dj_dw/m                                   #(n,)
-    dj_db = dj_db/m                                   #scalar
-        
-    return dj_db, dj_dw  
+    dj_dw = dj_dw/m  # (n,)
+    dj_db = dj_db/m  # scalar
+
+    return dj_db, dj_dw
 
 
-def gradient_descent(X, y, w_in, b_in, alpha, num_iters): 
+def gradient_descent(X, y, w_in, b_in, alpha, num_iters):
     """
     Performs batch gradient descent
-    
+
     Args:
       X (ndarray (m,n)   : Data, m examples with n features
       y (ndarray (m,))   : target values
@@ -110,30 +120,30 @@ def gradient_descent(X, y, w_in, b_in, alpha, num_iters):
       b_in (scalar)      : Initial values of model parameter
       alpha (float)      : Learning rate
       num_iters (scalar) : number of iterations to run gradient descent
-      
+
     Returns:
       w (ndarray (n,))   : Updated values of parameters
       b (scalar)         : Updated value of parameter 
     """
     # An array to store cost J and w's at each iteration primarily for graphing later
     J_history = []
-    w = copy.deepcopy(w_in)  #avoid modifying global w within function
+    w = copy.deepcopy(w_in)  # avoid modifying global w within function
     b = b_in
-    
+
     for i in range(num_iters):
         # Calculate the gradient and update the parameters
-        dj_db, dj_dw = compute_gradient_logistic(X, y, w, b)   
+        dj_db, dj_dw = compute_gradient_logistic(X, y, w, b)
 
         # Update Parameters using w, b, alpha and gradient
-        w = w - alpha * dj_dw               
-        b = b - alpha * dj_db               
-      
+        w = w - alpha * dj_dw
+        b = b - alpha * dj_db
+
         # Save the logistic cost J at each iteration
-        if i<100000:      # prevent resource exhaustion
+        if i < 100000:      # prevent resource exhaustion
             J_history.append(compute_cost_matrix(X, y, w, b, logistic=True))
 
         # Print cost every at intervals 10 times or as many iterations if < 10
-        if i% math.ceil(num_iters / 10) == 0:
+        if i % math.ceil(num_iters / 10) == 0:
             print(f"Iteration {i:4d}: Cost {J_history[-1]}   ")
-        
-    return w, b, J_history         #return final w,b and J history for graphing
+
+    return w, b, J_history  # return final w,b and J history for graphing
